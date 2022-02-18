7d2c919d9bcf37485284c307958d801e290be306
edodima13 <edodima13@libero.it>
2022-02-18 15:55:29 +0100

    avanzamneto

--- myjavascript.js
+++ myjavascript.js
@@ -32,5 +32,33 @@ const team = [
     },
   ];
   //recuperare il container per inserire i dati della card
-  const teamContainer = document.querySelector(".team-container")
+  const teamContainer = document.querySelector(".team-container");
+
+  //replicare le card con i dati dei membri
+  for (let i = 0; i < team.length; i++){
+    //dichiarare le variabili da replicare dell'oggetto
+    const membriDelTeam = team [i];
+    let nome = membriDelTeam.name;
+    let ruolo = membriDelTeam.role;
+    let immagine = membriDelTeam.image;
+    //console.log( nome, ruolo, immagine)
+
+    //crare l'elemento card dove introdurre le variabili
+    const card = `
+        <div class="team-card">
+            <div class="card-image">
+                <img
+                    src="img/${immagine}"
+                    alt="${nome}"
+                />
+            </div>
+            <div class="card-text">
+                <h3>${nome}</h3>
+                <p>${ruolo}</p>
+            </div>
+            </div>
+        </div>`
+        console.log(card)
+    //inserire l'elemento nel container nell'html
+  }
 
